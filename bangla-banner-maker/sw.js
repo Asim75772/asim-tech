a2cbccd8ac44bc756b8a7239d3c89ac6bd2b1143
Asim75772 <146651181+Asim75772@users.noreply.github.com>
2026-08-31 19:34:41 +0530

    Update service worker cache

--- bangla-banner-maker/sw.js
+++ bangla-banner-maker/sw.js
@@ -1,105 +1,45 @@
-const CACHE_NAME = "asim-tech-banner-maker-v2";
+const CACHE_NAME = "asim-tech-banner-v2";
 
-const APP_FILES = [
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json",
-  "./sw.js"
+  "./manifest.json"
 ];
 
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
 
-self.addEventListener(
-  "install",
-  event => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
 
-    event.waitUntil(
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
 
-      caches.open(CACHE_NAME)
-        .then(cache => {
-
-          return cache.addAll(APP_FILES);
-
-        })
-
-    );
-
-    self.skipWaiting();
-
-  }
-);
-
-
-self.addEventListener(
-  "activate",
-  event => {
-
-    event.waitUntil(
-
-      caches.keys()
-        .then(keys => {
-
-          return Promise.all(
-
-            keys
-              .filter(
-                key => key !== CACHE_NAME
-              )
-              .map(
-                key => caches.delete(key)
-              )
-
-          );
-
-        })
-
-    );
-
-    self.clients.claim();
-
-  }
-);
-
-
-self.addEventListener(
-  "fetch",
-  event => {
-
-    if(
-      event.request.method !== "GET"
-    ){
-      return;
-    }
-
-    event.respondWith(
-
-      caches.match(
-        event.request
-      )
-      .then(cached => {
-
-        if(cached){
-          return cached;
-        }
-
-        return fetch(
-          event.request
-        )
-        .then(response => {
-
-          return response;
-
-        })
-        .catch(() => {
-
-          return caches.match(
-            "./index.html"
-          );
-
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
         });
 
+        return response;
       })
-
-    );
-
-  }
-);
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
